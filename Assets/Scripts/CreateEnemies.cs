e469d0232e9c90679e1b446e3e6f7fb5ef14a785
Ian Everett <ian_everett@me.com>
2021-10-10 20:45:40 +0100

    Initial commit

---
+++ Assets/Scripts/CreateEnemies.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEnemies : MonoBehaviour
+{
+    public GameObject Object;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            float x_pos = Random.Range(-10, 10);
+            Instantiate<GameObject>(Object, new Vector2(x_pos, 5.0f), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private static int count;
+    private int id;
+
+
+    private void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        float gravity = Random.Range(0.05f, 0.3f);
+        float rotation = Random.Range(0, 360);
+
+        rb.rotation = rotation;
+        rb.gravityScale = gravity;
+        id = count++;
+        Debug.Log("Created " + id + " with gravity:" + gravity);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Floor")
+        {
+            if (count > 0)
+                count--;
+
+            Debug.Log("Enemy Destroyed:" + id + " Remaining:" + count);
+
+            Destroy(gameObject);
+        }
+    }
+}
